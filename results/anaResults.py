1bc2bb4517dd02b28ca7da5fd7fa37bc87389b6f
Gagik Gavalian <gavalian@Gagiks-MacBook-Air.local>
2020-08-25 19:09:09 -0400

    added a new script testing analysis in Jshell

--- results/anaResults.py
+++ results/anaResults.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-xaxis = ["C++","C++O2","Java","Groovy"]
-data2 = [42.8, 10.22, 11.29, 27.0]
-data1 = [37.8, 5.54, 4.78, 17.5]
+xaxis = ["C++","C++O2","Java","JShell","Groovy"]
+#data2 = [42.8, 10.22, 11.29, 27.0]
+data2 = [5, 4.68, 6.50, 8.42, 9.5]
+data1 = [37.8, 5.54, 4.78, 5.33, 17.5]
 #plt.ylim(0.0, 50)
 plt.xlabel("Languages")
 plt.ylabel("Execution Time (seconds)")
-plt.bar(xaxis, data1)
-plt.bar(xaxis, data2, bottom=data1)
+plt.bar(xaxis, data1, label='analysis')
+plt.bar(xaxis, data2, bottom=data1, label='I/O')
+plt.legend(loc="upper right")
 plt.show()
