d4631184322cbbfb93ac69fec948dd84444d04c9
Gagik Gavalian <gavalian@Gagiks-MacBook-Air.local>
2020-08-25 23:37:05 -0400

    added a fortran benchmark code

--- results/piResults.py
+++ results/piResults.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-xaxis = ["CINT","C++","C++O2","Java","Groovy"]
-data1 = [1.348, 2.096, 1.474, 0, 1.472]
-data2 = [0, 0, 0, 1.211, 0]
+xaxis = ["CINT","C++","C++O2","Fortran","Java","Groovy"]
+data1 = [1.348, 2.096, 1.474, 1.65, 0, 1.472]
+data2 = [0, 0, 0, 0.0, 1.211, 0]
 plt.ylim(0.0, 2.4)
 plt.xlabel("Languages")
 plt.ylabel("Execution Time (seconds)")
